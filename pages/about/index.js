5f08f7a081dc646bca6c5697fe87148deedf1c01
Alexandr <babiyksasha21@gmail.com>
2023-08-06 17:53:48 +0300

    index-3

--- pages/about/index.js
+++ pages/about/index.js
@@ -153,7 +153,7 @@ const About = () => {
             {aboutData.map((item, itemIndex) => {
               return (
                 <div
-                  key={itemIndex}
+                  key={item.title}
                   className={`${
                     index === itemIndex &&
                     'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
